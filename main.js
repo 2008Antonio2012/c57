8a0d7fc4391c51bf870f843e411669c61c1d2e51
2008Antonio2012 <142014749+2008Antonio2012@users.noreply.github.com>
2023-08-15 19:21:14 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function changecolor(){
+document.getElementById("btn").style.color="red";
+document.getElementById("btn").style.background="black";
+document.getElementById("btn").style.fontSize="30px";
+document.getElementById("i1").style.height="200px";
+}
